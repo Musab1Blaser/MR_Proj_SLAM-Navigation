70aa118b8de8802d74ef20f93dc76a8603c4cdee
Musab1Blaser <musab16000@gmail.com>
2024-12-03 23:22:42 +0500

    worlds setup - go through readme for setup on your machine

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -25,16 +25,27 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    # worlds: office, construction, maze
+    worlds = ['slam_maps/gazebo_models_worlds_collection-master/worlds/office_earthquake.world', 'slam_maps/gazebo_models_worlds_collection-master/worlds/office_cpr_construction.world', 'small_maze/smaze2d.world']
+    # Path to the world file (replace with your actual world file path)
+    world_file_path = os.path.join(get_package_share_directory(package_name), 'worlds', worlds[1])
+    print(world_file_path) # for debugging
 
+    # Include the Gazebo launch file with the specified world file
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        launch_arguments={'world': world_file_path}.items()
+    )
+    
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'differential_drive_robot'],
+                                   '-entity', 'differential_drive_robot',
+                                        '-x', '10.0',  # Set x-coordinate
+                                        '-y', '0.0',  # Set y-coordinate
+                                        '-z', '0.0',  # Set z-coordinate
+                                        '-Y', '1.57'  # Set yaw (e.g., 90 degrees in radians)
+                                    ],
                         output='screen')
 
     controller_node = Node(
